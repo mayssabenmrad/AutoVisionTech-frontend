e4cd00f2130a76e80fd6aae372a50b3224139f5b
Mohamed Affes <med123011@gmail.com>
2025-12-05 19:32:07 +0100

    feat: add hero component with animations and integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from '@shared/components/navbar/navbar';
 import { Footer } from '@shared/components/footer/footer';
 import { CarCrad } from './features/car-crad/car-crad';
+import { Hero } from '@shared/components/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer, CarCrad],
+  imports: [RouterOutlet, Navbar, Footer, CarCrad, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/shared/components/hero/hero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  templateUrl: './hero.html',
+  styleUrl: './hero.css',
+  standalone: true,
+})
+export class Hero {
+
+}
