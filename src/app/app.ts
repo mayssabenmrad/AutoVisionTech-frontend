c46e25456ee7508ce1e9d069ebdcaf2b2dfd450e
Mohamed Affes <med123011@gmail.com>
2025-12-05 18:44:08 +0100

    feat: add car card component to main content

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '@shared/components/navbar/navbar';
 import { Footer } from '@shared/components/footer/footer';
+import { CarCrad } from './features/car-crad/car-crad';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, CarCrad],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/features/car-crad/car-crad.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-crad',
+  imports: [],
+  templateUrl: './car-crad.html',
+  styleUrl: './car-crad.css',
+  standalone: true,
+})
+export class CarCrad {
+
+}
