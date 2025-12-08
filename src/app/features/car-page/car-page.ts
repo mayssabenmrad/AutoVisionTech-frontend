521028ceac57cbfa3959946a13958cf165f8d71f
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-12-08 16:55:28 +0100

    feat(carpage): implement retrieving car info dynamically

--- src/app/features/car-page/car-page.ts
+++ src/app/features/car-page/car-page.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { CarDetails } from '@shared/components/car-details/car-details';
 import { Comments } from '@shared/components/comments/comments';
 import { ReservationForm } from '@shared/components/reservation-form/reservation-form';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarService } from 'src/app/core/services/car.service';
+import { Car } from 'src/app/core/models';
 
 @Component({
   selector: 'app-car-page',
@@ -12,4 +15,31 @@ import { ReservationForm } from '@shared/components/reservation-form/reservation
   styleUrls: ['./car-page.css']
 })
 export class CarPage {
+  car: Car | null = null;
+  carId: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private carService: CarService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.carId = params.get('id') || '';
+      if (this.carId) {
+        this.loadCarData();
+      }
+    });
+  }
+
+  //load car data using car service
+  loadCarData() {
+    this.carService
+      .getCarById(this.carId) 
+      .subscribe((data) => {
+        this.car = data;
+      });
+  }
+  
+
 }
