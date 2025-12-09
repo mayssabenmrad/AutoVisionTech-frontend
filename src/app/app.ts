1c8922e7ff4d866608e85f9dd8ded1840e399bc1
Mohamed Affes <med123011@gmail.com>
2025-12-09 21:48:31 +0100

    feat(services): add services component with hero section, service listings, and benefits

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '@shared/components/navbar/navbar';
 import { Footer } from '@shared/components/footer/footer';
+import { Services } from './features/services/services';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer, Services],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
