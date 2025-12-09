71b400fa36c8669e39b90f7db612905c57f5a18a
Mohamed Affes <med123011@gmail.com>
2025-12-09 20:30:35 +0100

    feat(navbar): update navigation links and current page state for consistency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Signup } from './features/signup/signup';
 import { Signin } from './features/signin/signin';
 import { ManageReservations } from './features/manage-reservations/manage-reservations';
 import { CarPage } from './features/car-page/car-page';
+import { ProfilePage } from './features/profile-page/profile-page';
 
 export const routes: Routes = [
   { path: '', component: MainPage },
@@ -15,5 +16,6 @@ export const routes: Routes = [
   { path: 'signin', component: Signin },
   { path: 'manage-reservations', component: ManageReservations },
   { path: 'car-page/:id', component: CarPage },
+  { path: 'profile', component: ProfilePage },
   { path: '**', redirectTo: '' },
 ];
